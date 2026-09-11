e74975ec67fa6b3bd83bfd92e844bfb6121c35c9
Kamihates <36231247+Kamihates@users.noreply.github.com>
2026-09-11 10:32:24 +0200

    update: add redirected links

--- Assets/Project/Scripts/Menu/Steps/MainMenuStep.cs
+++ Assets/Project/Scripts/Menu/Steps/MainMenuStep.cs
@@ -7,6 +7,12 @@ public class MainMenuStep : MonoBehaviour
     [SerializeField] private Button playButton;
     [SerializeField] private Button settingsButton;
     [SerializeField] private Button quitButton;
+    [SerializeField] private Button IIMButton;
+    [SerializeField] private Button ItchButton;
+
+    [Header("Links")]
+    [SerializeField] private string IIMLink = "";
+    [SerializeField] private string ItchURL = "";
 
     [Header("Overlay")]
     [SerializeField] private SettingsPanel settingsPanel;
@@ -19,6 +25,8 @@ public class MainMenuStep : MonoBehaviour
         if (playButton != null) playButton.onClick.AddListener(Play);
         if (settingsButton != null) settingsButton.onClick.AddListener(OpenSettings);
         if (quitButton != null) quitButton.onClick.AddListener(Quit);
+        if (IIMButton != null) IIMButton.onClick.AddListener(OpenIIMLink);
+        if (ItchButton != null) ItchButton.onClick.AddListener(OpenItchLink);
     }
 
     private void OnDestroy()
@@ -26,6 +34,8 @@ public class MainMenuStep : MonoBehaviour
         if (playButton != null) playButton.onClick.RemoveListener(Play);
         if (settingsButton != null) settingsButton.onClick.RemoveListener(OpenSettings);
         if (quitButton != null) quitButton.onClick.RemoveListener(Quit);
+        if (IIMButton != null) IIMButton.onClick.RemoveListener(OpenIIMLink);
+        if (ItchButton != null) ItchButton.onClick.RemoveListener(OpenItchLink);
     }
 
     private void Play() => ScreenFader.GoToScene(gameSceneName);
@@ -33,6 +43,22 @@ public class MainMenuStep : MonoBehaviour
     private void OpenSettings()
     {
         if (settingsPanel != null) settingsPanel.Open();
+    }
+
+    private void OpenIIMLink()
+    {
+        if (!string.IsNullOrEmpty(IIMLink))
+        {
+            Application.OpenURL(IIMLink);
+        }
+    }
+
+    private void OpenItchLink()
+    {
+        if (!string.IsNullOrEmpty(ItchURL))
+        {
+            Application.OpenURL(ItchURL);
+        }
     }
 
     private void Quit()
